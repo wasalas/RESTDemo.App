1bda7330bbc8ed443903632dac71c16bff6fbb29
walther salas <wasalas@hotmail.com>
2023-03-03 08:36:06 -0500

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace RESTDemo;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = new MainViewModel();
+	}
+
+	
+}
+
---
+++ MainViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RESTDemo
+{
+    public class MainViewModel
+    {
+        HttpClient client;
+        JsonSerializerOptions _serializerOptions;
+        string baseUrl = "https://6401e3940a2a1afebef44dac.mockapi.io";
+        private List<User> Users;
+
+        public MainViewModel()
+        {
+            client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+        }
+
+        public ICommand GetAllUsersCommand =>
+            new Command(async () =>
+            {
+                var url = $"{baseUrl}/users";
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // var content = await response.Content.ReadAsStringAsync();
+                    using (var responseStream =
+                        await response.Content.ReadAsStreamAsync())
+                    {
+                        var data = await JsonSerializer
+                        .DeserializeAsync<List<User>>(responseStream, _serializerOptions);
+                        Users = data;
+                    }
+                }
+            });
+        public ICommand GetSingleUserCommand =>
+            new Command(async () =>
+            {
+                var url = $"{baseUrl}/users/25";
+                var response = await client.GetStringAsync(url);
+            });
+        public ICommand AddUserCommand =>
+            new Command(async () =>
+            {
+                var url = $"{baseUrl}/users";
+                var user =
+                    new User
+                    {
+                        createdAt = DateTime.Now,
+                        name = "Walther Salas",
+                        avatar = "https://fakeimg.pl/350*200/?text=MAUI"
+                    };
+                string json = JsonSerializer.Serialize<User>(user, _serializerOptions);
+
+                StringContent content = new StringContent(json, Encoding.UTF8,"application/json");
+
+                var response = await client.PostAsync(url, content);
+            });
+
+        public ICommand UpdateUserCommand =>
+            new Command(async () =>
+            {
+                var user = Users.FirstOrDefault(x => x.id == "1");
+                
+                var url = $"{baseUrl}/users/1";
+
+                user.name = "Albert Einstein";
+
+                string json = JsonSerializer.Serialize<User>(user, _serializerOptions);
+
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PutAsync(url, content);
+            });
+
+        public ICommand DeleteUserCommand =>
+            new Command(async () =>
+            {                
+                var url = $"{baseUrl}/users/51";
+                var response = await client.DeleteAsync(url);
+            });
+    }
+}
